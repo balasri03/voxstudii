aa16acbb4960539130e5e97ecbab9587746a0915
balasri <n210342@rguktn.ac.in>
2025-06-21 20:25:06 +0530

    commited

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,11 @@ import cors from 'cors'
 import feedbackRoutes from './routes/studentfeedback.router.js'
 import adminloginRouter from './routes/admin_login.router.js'
 
+
+app.use(cors({
+  origin: "https://voxstudii-n2tf.vercel.app/feedback", // your Vercel frontend URL
+  credentials: true,
+}));
 const app = express()
 app.use(cors())
 app.use(express.json())
